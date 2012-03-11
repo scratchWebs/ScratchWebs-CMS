e14f384e8a2377b388b0d0996668168fae0b0e5e
Tom Harvey <tomhrvy@gmail.com>
2012-03-11 12:11:28 +0000

    fixed future conflict issue with constants.php

--- cms/cms.php
+++ cms/cms.php
@@ -1,10 +1,14 @@
 <?php session_start();
-// This file "cms.php" should be required_once on every document which needs to use the CMS
+// This file should be required_once on every document which needs to use the CMS
 
 error_reporting(0);
 //error_reporting(-1);
 //error_reporting(E_ERROR | E_WARNING | E_PARSE);
 //error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+define('PATH_CORE', dirname(__FILE__) . '/core/');
+define('PATH_CONTROLS', dirname(__FILE__) . '/controls/');
+define('PATH_CMS', dirname(__FILE__) . '/');
 
 require_once("core/constants.php");
 require_once("core/dbConnect.php");
