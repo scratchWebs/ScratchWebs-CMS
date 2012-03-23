aabd3fd85c89343c52937c0898caa2b7d2ff638b
Luke Davies <looshus@gmail.com>
2012-03-23 18:19:13 +0000

    ignore constants.php

--- cms/core/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DOCUMENT_ROOT','/cms-master/');			  // webserver path (to the cms folder)
-
-define('PATH_HTML', dirname(__FILE__) . '/../../');		  // local path (to the customers website)
-define('PATH_LOCAL', dirname(__FILE__) . '/../../../');	  // local path (to a directory not accessible via the web)
-
-?>
