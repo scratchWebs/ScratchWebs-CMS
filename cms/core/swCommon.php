ec99d4432239768cea595fa9f385f5335031ac41
Tom Harvey <tomhrvy@gmail.com>
2013-01-30 21:50:56 +0000

    Fixed #27 (Again) - Remove microsoft-enabled quotes

--- cms/core/swCommon.php
+++ cms/core/swCommon.php
@@ -32,19 +32,7 @@ class swCommon
 	}
 	public static function convert_smart_quotes($string)
 	{
-		$search = array(chr(145),
-						chr(146),
-						chr(147),
-						chr(148),
-						chr(151));
-		
-		$replace = array('&lsquo;',
-						 '&rsquo;',
-						 '&ldquo;',
-						 '&rdquo;',
-						 '&mdash;');
-		
-		return str_replace($search, $replace, $string);
+		return iconv('UTF-8', 'ASCII//TRANSLIT', $string);
 	}
 }
 
